2d30834bb985f30d372719d54c98569cf54b63a4
hey-sushant <prakash.sushant11@gmail.com>
2025-07-15 16:21:10 +0530

    without loading animation

--- MIRANDA/js/script.js
+++ MIRANDA/js/script.js
@@ -4,3 +4,77 @@ gsap.to('#slide-container h1',{
     repeat:-1,
     ease: "none",
 })
+
+
+let firstBox = document.querySelector('#h-box-container #first-h-box')
+let secondBox = document.querySelector('#h-box-container #second-h-box')
+let thirdBox = document.querySelector('#h-box-container #third-h-box')
+
+
+firstBox.addEventListener('mouseenter',function(){
+    gsap.to('#h-box-container #second-h-box',{
+        x:'23vw',
+        ease:'power2.inOut',
+        duration:0.8
+    })
+    gsap.to('#h-box-container #third-h-box',{
+        x:'23vw',
+        ease:'power2.inOut',
+        duration:0.8
+    })
+    gsap.to('#h-box-container #forth-h-box',{
+        x:'23vw',
+        ease:'power2.inOut',
+        duration:0.8
+    })
+})
+firstBox.addEventListener('mouseleave',function(){
+    gsap.to('#h-box-container #second-h-box',{
+        x:0,
+    })
+    gsap.to('#h-box-container #third-h-box',{
+        x:0,
+    })
+    gsap.to('#h-box-container #forth-h-box',{
+        x:0,
+    })
+})
+
+
+secondBox.addEventListener('mouseenter',function(){
+    gsap.to('#h-box-container #third-h-box',{
+        x:'21vw',
+        ease:'power2.inOut',
+        duration:0.8
+    })
+    gsap.to('#h-box-container #forth-h-box',{
+        x:'21vw',
+        ease:'power2.inOut',
+        duration:0.8
+    })
+})
+secondBox.addEventListener('mouseleave',function(){
+    gsap.to('#h-box-container #third-h-box',{
+        x:0
+    })
+    gsap.to('#h-box-container #forth-h-box',{
+        x:0
+    })
+})
+
+
+thirdBox.addEventListener('mouseenter',function(){
+    gsap.to('#h-box-container #forth-h-box',{
+        x:'21.5vw',
+        ease:'power2.inOut',
+        duration:0.8
+    })
+})
+thirdBox.addEventListener('mouseleave',function(){
+    gsap.to('#h-box-container #forth-h-box',{
+        x:0
+    })
+})
+
+
+
